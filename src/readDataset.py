f7bf59f89412683652865c348a24ff1cdfec2191
Dhruv Patel <dhruvkumarr.patel51@gmail.com>
2018-11-25 23:29:58 -0600

    read dataset

---
+++ src/readDataset.py
@@ -0,0 +1,17 @@
+import os
+import tensorflow as tf
+
+def read_images_from_dirs(dataset_path):
+    images=[]
+    labels=[]
+    for root, dirs, files in os.walk(dataset_path):  
+        for dirname in dirs:
+            for filename in os.listdir(os.path.join(root,dirname)):
+                if filename.endswith("jpg"):
+                    img=tf.read_file(os.path.join(root,dirname,filename))
+                    image = tf.image.decode_jpeg(img, channels=3)
+                    images.append(image)
+                    labels.append(dirname)
+    images=tf.convert_to_tensor(images)
+    labels=tf.convert_to_tensor(labels)
+    return [images,labels]
